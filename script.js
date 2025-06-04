07b0a129a848c23c7e52c2d59dda0e2eb9b1aaec
David Wanderley <davidgomeswanderley@gmail.com>
2025-06-03 22:06:26 -0300

    adicionando o calculo de kelvin e criando o arquivo css

--- script.js
+++ script.js
@@ -1,18 +1,32 @@
 function converterCtoF(tempC) {
     const tempF = (tempC * 9/5) + 32
-    console.log(`Temperatura = ${tempF} F`);
-
     return tempF;
 }
 
 function convertCtoK(tempC) {
-    
+    const tempK = tempC + 273.15;
+    return tempK;
 }
 
 
-function converter() {
-    let tempC = document.getElementById("tempC").value;
-    const resultado = converterCtoF(tempC);
+function converter(temp) {
+    let tempC = document.getElementById("tempC").value.trim() === "" ? 0 : parseFloat(document.getElementById("tempC").value);
 
-    document.getElementById("resultado").innerHTML = resultado;
+    if (temp === "k") {
+        const resultadok = convertCtoK(tempC);
+        document.getElementsByClassName("resultado")[0].style.display = 'flex';
+        return document.getElementById("resultado").innerHTML = ` ${resultadok} °K`;
+    }else if (temp === "f") {
+        const resultadof = converterCtoF(tempC);
+        document.getElementsByClassName("resultado")[0].style.display = 'flex';
+        return document.getElementById("resultado").innerHTML = ` ${resultadof} °F`;
+    } else {
+        alert("Selecione uma opção válida.");
+        return;
+    }
+}
+
+function limpar() {
+    document.getElementsByClassName("resultado")[0].style.display = 'none';
+
 }
